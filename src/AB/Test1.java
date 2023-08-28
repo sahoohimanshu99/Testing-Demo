3b2eca41a1f8e61a32274d639b83a8dd72996752
HIMANSHU SEKHAR <HIMANSHU SEKHAR@DESKTOP-MRO84KU>
2023-08-29 01:11:57 +0530

    First commit....

---
+++ src/AB/Test1.java
@@ -0,0 +1,18 @@
+package AB;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		String hungry = "yes";
+		
+		if (hungry == "yes")
+			System.out.println("eat something....");
+		else {
+			System.out.println("Do work.....");
+		}
+			
+
+	}
+
+}
